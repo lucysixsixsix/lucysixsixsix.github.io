003fd17efb1b0e44f5e47720e038ad162e794530
lucy <36128102+lucysixsixsix@users.noreply.github.com>
2025-10-25 12:33:42 +0200

    Delete nav.js

--- nav.js
+++
@@ -1,7 +0,0 @@
-const $ = x => document.querySelector(x);
-
-function dropDown() {
-  const dd = $("#drop-down");
-  dd.classList.toggle("hidden");
-  $("#indicator").innerHTML = dd.classList == "hidden" ? "↓" : "↑";
-};
