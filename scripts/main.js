d927284e8ee2ec589aea803bf8baa82cfb3f09d4
lucy <36128102+lucysixsixsix@users.noreply.github.com>
2025-10-25 17:43:55 +0200

    Delete scripts directory

--- scripts/main.js
+++
@@ -1,9 +0,0 @@
-let f=k=>(k<10)?("0"+k):k;setInterval(()=>{let d=new Date();document.querySelector(".clock").innerText=`${f(d.getHours())}:${f(d.getMinutes())}:${f(d.getSeconds())}`;},1000);
-
-const $ = x => document.querySelector(x);
-
-function dropDown() {
-  const dd = $("#drop-down");
-  dd.classList.toggle("hidden");
-  $("#indicator").innerHTML = dd.classList == "hidden" ? "↓" : "↑";
-};
